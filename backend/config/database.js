35480352d63e0667a636dd412d2c865024239561
subham123-ui <suvam.rockstar@gmail.com>
2024-04-17 22:58:43 +0530

    backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect("mongo://localhost")
+mongoose.connect("mongo://localhost", {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true}).then((data)=>{
+    console.log(`MongoDb Connected with Server: ${data.connection.host}`);
+}).catch((err)=>{
+    console.log(err);
+})
