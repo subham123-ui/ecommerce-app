68f79a38e845cf6c1587854598856aae64e8c5b2
subham123-ui <suvam.rockstar@gmail.com>
2024-04-23 00:00:07 +0530

    backnd

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,7 +23,7 @@ exports.getAllProducts = async (req, res) => {
 
 exports.getProductDetails = async(req,res) =>{
 
-    const product = await Product.findById(req.params,id)
+    const product = await Product.findById(req.params.id)
 
     if (!product) {
         return res.status(500).json({
